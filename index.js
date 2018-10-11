d0b7d127aa8b89301f8a27759ffe17f7a18095dc
janerishbaluca <janerishbaluca@gmail.com>
2018-10-11 23:22:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var array = []
+  for (let i=0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(array) {
+  var facts = [];
+  let i = 0;
+  while (i < array.length) {
+    facts.push(`${array[i]}!!!`);
+    i++;
+  }
+  return facts
+}
